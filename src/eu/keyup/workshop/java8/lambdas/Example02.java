1c91ca46e874b7ce9552afc9c2a54a75ab9911f5
Ondra Kratochvíl <ondrej.kratochvil@keyup.eu>
2015-10-19 22:15:43 +0200

    Additional FI and Lambda examples

--- src/eu/keyup/workshop/java8/lambdas/Example02.java
+++ src/eu/keyup/workshop/java8/lambdas/Example02.java
@@ -1,6 +1,9 @@
 package eu.keyup.workshop.java8.lambdas;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * @author Ondřej Kratochvíl
@@ -18,19 +21,19 @@ public class Example02 {
         // solutions: try-catch/add exception to methods declaration/use other functional interface
 
         // Method reference
-        PowerCalculator calc1 = (x,y) -> Math.pow(x, y);
+        PowerCalculator calc1 = (x, y) -> Math.pow(x, y);
         PowerCalculator calc2 = Math::pow;
 
         // getting the value - old way
         double oldWay = new PowerCalculator() {
             @Override
             public double calculate(double x, double y) {
-                return Math.pow(x,y);
+                return Math.pow(x, y);
             }
-        }.calculate(1,2);
+        }.calculate(1, 2);
 
         // getting the value - new way
-        double newWay = ((PowerCalculator)Math::pow).calculate(1,2);
+        double newWay = ((PowerCalculator) Math::pow).calculate(1, 2);
 
         // this and super reference
         // toDo: think of example using this::foo() and super::foo()
