ee750bc991b2838e2e9d9b9dafdf70cda9499c93
novoty <jakub.novotny@keyup.eu>
2015-10-21 23:10:13 +0200

    range spliterator solution

--- src/eu/keyup/workshop/java8/stream/Example32OwnRangeSpliterator.java
+++ src/eu/keyup/workshop/java8/stream/Example32OwnRangeSpliterator.java
@@ -5,47 +5,60 @@ import eu.keyup.workshop.java8.helper.Helper;
 import java.util.List;
 import java.util.Spliterator;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class Example32OwnRangeSpliterator {
 
     public static void main(String[] args) {
 
-        // todo: create stream and print numbers
-
+        List<Integer> list = StreamSupport.stream(new RangeSpliterator(0, 20), true)
+                .collect(Collectors.toList());
+        System.out.println(list);
     }
 
     private static class RangeSpliterator implements Spliterator<Integer> {
 
         private int from;
         private int to;
+        private int cursor;
 
         private RangeSpliterator(int from, int to) {
             this.from = from;
             this.to = to;
+            cursor = from;
         }
 
         // ******************** BASIC IMPLEMENTED METHODS ********************
 
-        // todo: implement
 
         @Override
         public boolean tryAdvance(Consumer<? super Integer> action) {
-            return false;
+            action.accept(cursor);
+            cursor ++;
+            return cursor < to;
         }
 
         @Override
         public Spliterator<Integer> trySplit() {
-            return null;
+            int diff = (to - cursor) / 2;
+            if (diff > 5) {
+                int prevCursor = cursor;
+                cursor += diff;
+                return new RangeSpliterator(prevCursor, cursor);
+            } else {
+                return null;
+            }
         }
 
         @Override
         public long estimateSize() {
-            return 0;
+            return to - from;
         }
 
         @Override
         public int characteristics() {
-            return 0;
+            return ORDERED | DISTINCT;
         }
     }
 
